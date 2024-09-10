5d78b798b9185fc2b845117b9e176f0c90c5e38d
ramaditya31 <rama.aditya31@ui.ac.id>
2024-09-11 01:57:07 +0700

    done

--- jersey/settings.py
+++ jersey/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,10 @@ WSGI_APPLICATION = 'jersey.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="postgres://default:0TqrHlNhkdD5@ep-misty-unit-a1tw2xjp.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
+        conn_max_age=600,
+    )
 }
 
 
